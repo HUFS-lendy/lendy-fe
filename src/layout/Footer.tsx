a3d53514e55fa9c35526b071143510b1f670367d
yiseoffline <lsy0476@hufs.ac.kr>
2026-05-17 19:08:32 +0900

    feat: favicon edited

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,20 +1,14 @@
-// import { useNavigate } from "react-router-dom";
-
 const Footer = () => {
-  // const navigate = useNavigate();
-
   return (
     <div className="space-y-4 pt-10 md:space-y-0 bg-[#060a0c] text-white px-4">
-      <img
-        className="w-50 h-10"
-        src="https://computer.hufs.ac.kr/sites/computer/masterSkin/computer_JW_MS_K2WT001_M/images/logo_footer.svg"
-        alt="한국외국어대학교"
-      />
+      <a href="https://hufs.ac.kr" target="_blank" rel="noopener noreferrer">
+        <img
+          className="w-50 h-10"
+          src="https://computer.hufs.ac.kr/sites/computer/masterSkin/computer_JW_MS_K2WT001_M/images/logo_footer.svg"
+          alt="한국외국어대학교"
+        />
+      </a>
       <div className="flex flex-col items-end pb-4 space-y-2 text-xs text-right">
-        <div className="flex space-x-2">
-          <div className="font-bold">ADDRESS</div>
-          <div>경기도 용인시 처인구 모현읍 외대로 81</div>
-        </div>
         <div className="flex space-x-4">
           <div className="flex space-x-2">
             <div className="font-bold">TEL</div>
@@ -24,6 +18,10 @@ const Footer = () => {
             <div className="font-bold">EMAIL</div>
             <div>ces@hufs.ac.kr</div>
           </div>
+        </div>
+        <div className="flex space-x-2">
+          <div className="font-bold">ADDRESS</div>
+          <div>경기도 용인시 처인구 모현읍 외대로 81 공학관 205호</div>
         </div>
         {/* <div
           onClick={() => navigate("/admin")}
